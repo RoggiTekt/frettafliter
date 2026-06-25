6904996eeaf83c396492bdefc13174e345bdf4ad
RoggiTekt <sel04rognvalduro@gmail.com>
2026-06-25 21:58:07 +0000

    Update service worker shell version to v4

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const SHELL = "frettvaldur-v3";
+const SHELL = "frettvaldur-v4";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(ASSETS))); self.skipWaiting();
